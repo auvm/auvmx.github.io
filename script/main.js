ddbc7c004135109871f4b037c4a3f1c21fb016f1
auvm <angelurielvelascom@gmail.com>
2023-06-02 23:17:20 -0500

    add menu autoclose and links to each card

--- script/main.js
+++ script/main.js
@@ -1,8 +1,14 @@
 const menu_icon_img = document.querySelector(".menu_icon");
 const menu_container = document.querySelector(".menu_container");
+const list_item_menu = document.querySelectorAll(".list_item_menu");
 
 
 menu_icon_img.addEventListener("click", toggle_mobile_menu);
+
+list_item_menu.forEach((item) => {
+    item.addEventListener("click", toggle_mobile_menu);
+})
+
 
 function toggle_mobile_menu(){
     // let isShoppingCartOpen = !shoppingCart.classList.contains("inactive");
